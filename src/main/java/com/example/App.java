75b33a42ba450999a19e13813cefa413fa35ac1e
Derin Girish <girishderin@gmail.com>
2025-03-14 14:44:51 +0530

    second commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,25 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Line " + i);
+        }
+        
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum += i;
+            System.out.println("Sum after adding " + i + " is: " + sum);
+        }
+        
+        System.out.println("Final Sum: " + sum);
+        
+        if (sum > 10) {
+            System.out.println("The sum is greater than 10");
+        } else {
+            System.out.println("The sum is 10 or less");
+        }
     }
 }
